de846cee21419cd79c35743b7cde5ee07338dbfe
josh-jacobsen <joshjacobsen5@gmail.com>
2017-11-23 07:44:52 -0700

    Add setup-env-vars script

---
+++ config/setup-env-vars.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path')
+
+const readPath = path.resolve(__dirname, '../.sample-env')
+const writePath = path.resolve(__dirname, '../.env')
+
+fs.createReadStream(readPath)
+  .pipe(fs.createWriteStream(writePath));
