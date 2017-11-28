f332b4861df4f40a909750cdc43c3354d2ecbb05
josh-jacobsen <joshjacobsen5@gmail.com>
2017-11-29 07:07:06 +1300

    Call local endpoint with axios and json-server

--- build/dev-server.js
+++ build/dev-server.js
@@ -4,7 +4,44 @@ const fs = require('fs')
 const path = require('path')
 require('dotenv').config()
 
-console.log(process.env.BASIC)
+// dev axios work
+
+// import { appService } from '../src/service/service'
+// import appService from '../service/service.js'
+// const appService = require ('../src/service/service')
+
+// var axios = require('axios')
+
+// const apiKey = process.env.API_KEY
+// const termToSearch = '?q=plastics%20recycling'
+// const API_KEY = `d6b69f0511e544778394a4edc672be75`
+
+// console.log(apiKey)
+
+// axios.defaults.baseURL = 'https://newsapi.org/v2/everything'
+// axios.defaults.baseURL = 'http://localhost:3004'
+
+// const appService = {
+//   getPosts (queryString) {
+//     return new Promise((resolve) => {
+//       axios.get(`${queryString}`)
+//       // , {
+//       //   headers: {"x-api-key": API_KEY }
+//       // })
+//       .then(response => {
+//         resolve(response.data)
+//       })
+//     })
+//   }
+// }
+
+// const query = '?q=plastics%20recycling'
+// const query = 'posts'
+// const result = appService.getPosts(query).then(data => {
+//   console.log(data)
+// })
+// console.log('getting data')
+// // console.log(appService.getPosts(query))
 
 const port = process.env.PORT || 3000
 
--- config/setup-env-vars.js
+++ config/setup-env-vars.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
+const fs = require('fs')
 const path = require('path')
 
 const readPath = path.resolve(__dirname, '../.sample-env')
 const writePath = path.resolve(__dirname, '../.env')
 
 fs.createReadStream(readPath)
-  .pipe(fs.createWriteStream(writePath));
+  .pipe(fs.createWriteStream(writePath))
