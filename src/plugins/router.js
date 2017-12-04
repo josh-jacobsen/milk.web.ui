a3117b773243572058956421857302d21d87fbb0
Josh Jacobsen <josh.jacobsen@xero.com>
2017-12-05 08:11:02 +1300

    Lazy loading compatible with windows

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -7,10 +7,16 @@ import VueRouter from 'vue-router'
 // import NotFound from '../theme/NotFound.vue'
 
 // This async loading allows for lazy loading, so components are
-// loaded only when the route is visited
-const Category = () => System.import('../theme/Category.vue')
-const Login = () => System.import('../theme/Login.vue')
-const NotFound = () => System.import('../theme/NotFound.vue')
+// loaded only when the route is visitedn
+// These ones do't work with windows
+// const Category = () => System.import('../theme/Category.vue')
+// const Login = () => System.import('../theme/Login.vue')
+// const NotFound = () => System.import('../theme/NotFound.vue')
+
+// These async loading patterns work on windows
+const Category = () => import('../theme/Category.vue')
+const Login = () => import('../theme/Login.vue')
+const NotFound = () => import('../theme/NotFound.vue')
 
 Vue.use(VueRouter)
 
